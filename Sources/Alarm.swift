ce63577eebfb70b32801c657d02c673328be1074
Kilian Koeltzsch <me@kilian.io>
2016-05-13 00:22:11 +0200

    Create Alarm

---
+++ Sources/Alarm.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+struct Alarm {
+    var otherAttrs = [String:String]()
+}
+
+extension Alarm: CalendarComponent {
+    mutating func addAttribute(attr: String, _ value: String) {
+        switch attr {
+            default:
+                otherAttrs[attr] = value
+        }
+    }
+
+    func toICal() -> String {
+        var str = "BEGIN:VALARM\n"
+
+        for (key, val) in otherAttrs {
+            str += "\(key):\(val)\n"
+        }
+
+        str += "END:VALARM\n"
+        return str
+    }
+}
